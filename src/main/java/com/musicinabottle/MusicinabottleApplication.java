b34f8c7687d02e3b39dd3c207192ba67ef499030
2jun0 <soo28819@gmail.com>
2024-05-20 15:45:00 +0900

    chore: setup spring boot project

---
+++ src/main/java/com/musicinabottle/MusicinabottleApplication.java
@@ -0,0 +1,13 @@
+package com.musicinabottle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicinabottleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicinabottleApplication.class, args);
+	}
+
+}

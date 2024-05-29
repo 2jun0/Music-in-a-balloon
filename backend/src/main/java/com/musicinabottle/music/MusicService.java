ff7478854c04a61e386410c9d809676fa9a1b89e
2jun0 <soo28819@gmail.com>
2024-05-29 17:57:45 +0900

    fix: @Transactional 누락

--- backend/src/main/java/com/musicinabottle/music/MusicService.java
+++ backend/src/main/java/com/musicinabottle/music/MusicService.java
@@ -12,8 +12,10 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class MusicService {
     private final Map<StreamingMusicType, StreamingMusicUrlParser> urlParsers;
     private final YoutubeMusicService youtubeMusicService;

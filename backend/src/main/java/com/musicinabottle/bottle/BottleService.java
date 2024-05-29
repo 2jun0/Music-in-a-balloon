014f5ec7c25da593fbaca66e2bfa4636fadcc830
2jun0 <soo28819@gmail.com>
2024-05-29 17:56:22 +0900

    fix: 병 엔티티를 생성한 뒤 영속화

--- backend/src/main/java/com/musicinabottle/bottle/BottleService.java
+++ backend/src/main/java/com/musicinabottle/bottle/BottleService.java
@@ -3,29 +3,37 @@ package com.musicinabottle.bottle;
 import com.musicinabottle.music.streaming.StreamingMusicType;
 import com.musicinabottle.music.streaming.spotify.SpotifyMusic;
 import com.musicinabottle.music.streaming.youtube.YoutubeMusic;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class BottleService {
     private final BottleRepository bottleRepository;
 
     public Bottle createYoutubeMusicBottle(YoutubeMusic youtubeMusic) {
-        return Bottle.builder()
+        Bottle bottle = Bottle.builder()
                 .uploadedStreamingMusicType(StreamingMusicType.YOUTUBE_MUSIC)
                 .youtubeMusic(youtubeMusic)
                 .build();
+        bottleRepository.save(bottle);
+        return bottle;
     }
 
     public Bottle createSpotifyMusicBottle(SpotifyMusic spotifyMusic) {
-        return Bottle.builder()
+        Bottle bottle = Bottle.builder()
                 .uploadedStreamingMusicType(StreamingMusicType.SPOTIFY_MUSIC)
                 .spotifyMusic(spotifyMusic)
                 .build();
+        bottleRepository.save(bottle);
+        return bottle;
     }
 
     public Bottle pickRandomBottle() {
-        return bottleRepository.findById(1L).orElseThrow();
+        List<Bottle> bottles = bottleRepository.findAll();
+        return bottles.getFirst();
     }
 }

658c19412ae1bab304ca1ba16df7d8c3350bb09e
2jun0 <soo28819@gmail.com>
2024-05-31 00:13:01 +0900

    feat: 유저는 쿠키로 인증한다

---
+++ backend/src/main/java/com/musicinabottle/auth/AuthenticationInterceptor.java
@@ -0,0 +1,25 @@
+package com.musicinabottle.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    private final AuthenticationContext context;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        for (var cookie : request.getCookies()) {
+            if (cookie.getName().equals("userId")) {
+                context.setUserId(Long.parseLong(cookie.getValue()));
+            }
+        }
+
+        return true;
+    }
+}

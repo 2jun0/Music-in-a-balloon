6c7d690e4dc552dee09f4b7398d8c2ce8b1b3410
2jun0 <soo28819@gmail.com>
2024-06-04 21:59:18 +0900

    test: Bottle 통합 테스트 리팩토링

--- backend/src/test/java/com/musicinabottle/bottle/BottleControllerTest.java
+++ backend/src/test/java/com/musicinabottle/bottle/BottleControllerTest.java
@@ -1,40 +1,31 @@
 package com.musicinabottle.bottle;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musicinabottle.IntegrationTest;
 import com.musicinabottle.bottle.request.CreateBottleRequest;
 import com.musicinabottle.bottle.response.BottleResponse;
 import com.musicinabottle.music.streaming.StreamingMusicType;
-import com.musicinabottle.user.User;
-import com.musicinabottle.user.UserRepository;
-import jakarta.servlet.http.Cookie;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class BottleControllerTest {
+class BottleControllerTest extends IntegrationTest {
 
-    @Autowired
-    protected ObjectMapper objectMapper;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private BottleRepository bottleRepository;
-    @Autowired
-    private UserRepository userRepository;
+    private ExtractableResponse<Response> postBottle(CreateBottleRequest request) {
+        return RestAssured
+                .given()
+                .cookie("userId", defaultUser.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/bottle")
+                .then().log().all().extract();
+    }
 
     @Test
     @DisplayName("병을 줍는다")
@@ -43,50 +34,42 @@ class BottleControllerTest {
     }
 
     @Test
-    @Transactional
     @DisplayName("유튜브 음악 URL로 병을 생성한다")
-    void createBottleByYoutubeMusicUrl() throws Exception {
-        var user = User.builder()
-                .name("username").build();
-        userRepository.save(user);
+    void createBottleByYoutubeMusicUrl() {
+        CreateBottleRequest request = new CreateBottleRequest("https://music.youtube.com/watch?v=n7ePZLn9_lQ");
+        String title = "Super Shy";
 
-        var request = new CreateBottleRequest("https://music.youtube.com/watch?v=n7ePZLn9_lQ");
-        var response = new BottleResponse(1L, "Super Shy", StreamingMusicType.YOUTUBE_MUSIC.name(), null);
+        ExtractableResponse<Response> response = postBottle(request);
+        BottleResponse bottleResponse = response.as(BottleResponse.class);
 
-        mockMvc.perform(post("/bottle")
-                        .cookie(new Cookie("userId", user.getId().toString()))
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(response.id()))
-                .andExpect(jsonPath("$.title").value(response.title()))
-                .andExpect(jsonPath("$.uploadedStreamingMusicType").value(response.uploadedStreamingMusicType()))
-                .andReturn();
-
-        var saved = bottleRepository.findById(response.id()).orElseThrow();
-        assert saved.getYoutubeMusic().getTitle().equals(response.title());
-        assert saved.getUploadedStreamingMusicType().name().equals(response.uploadedStreamingMusicType());
+        assertSoftly(
+                softly -> {
+                    softly.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
+                    softly.assertThat(bottleResponse.id()).isNotNull();
+                    softly.assertThat(bottleResponse.title()).isEqualTo(title);
+                    softly.assertThat(bottleResponse.uploadedStreamingMusicType()).isEqualTo(StreamingMusicType.YOUTUBE_MUSIC.name());
+                    softly.assertThat(bottleResponse.albumImageUrl()).isNotNull();
+                }
+        );
     }
 
     @Test
-    @Transactional
     @DisplayName("스포티파이 음악 URL로 병을 생성한다")
-    void createBottleBySpotifyMusicUrl() throws Exception {
-        var request = new CreateBottleRequest("https://open.spotify.com/track/5sdQOyqq2IDhvmx2lHOpwd");
-        var response = new BottleResponse(1L, "Super Shy", StreamingMusicType.SPOTIFY_MUSIC.name(), null);
+    void createBottleBySpotifyMusicUrl() {
+        CreateBottleRequest request = new CreateBottleRequest("https://open.spotify.com/track/5sdQOyqq2IDhvmx2lHOpwd");
+        String title = "Super Shy";
 
-        mockMvc.perform(post("/bottle")
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(response.id()))
-                .andExpect(jsonPath("$.title").value(response.title()))
-                .andExpect(jsonPath("$.uploadedStreamingMusicType").value(response.uploadedStreamingMusicType()));
+        ExtractableResponse<Response> response = postBottle(request);
+        BottleResponse bottleResponse = response.as(BottleResponse.class);
 
-        var saved = bottleRepository.findById(response.id()).orElseThrow();
-        assert saved.getSpotifyMusic().getTitle().equals(response.title());
-        assert saved.getUploadedStreamingMusicType().name().equals(response.uploadedStreamingMusicType());
+        assertSoftly(
+                softly -> {
+                    softly.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
+                    softly.assertThat(bottleResponse.id()).isNotNull();
+                    softly.assertThat(bottleResponse.title()).isEqualTo(title);
+                    softly.assertThat(bottleResponse.uploadedStreamingMusicType()).isEqualTo(StreamingMusicType.SPOTIFY_MUSIC.name());
+                    softly.assertThat(bottleResponse.albumImageUrl()).isNotNull();
+                }
+        );
     }
 }

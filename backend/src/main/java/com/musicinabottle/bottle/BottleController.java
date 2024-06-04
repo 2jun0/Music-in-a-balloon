9bbfbdf6f9897aeec937c4951197b5223c52853f
2jun0 <soo28819@gmail.com>
2024-06-04 21:58:01 +0900

    feat: POST /bottle은 201 상태코드를 반환

--- backend/src/main/java/com/musicinabottle/bottle/BottleController.java
+++ backend/src/main/java/com/musicinabottle/bottle/BottleController.java
@@ -4,8 +4,10 @@ import com.musicinabottle.auth.UserId;
 import com.musicinabottle.bottle.request.CreateBottleRequest;
 import com.musicinabottle.bottle.response.BottleResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,6 +21,7 @@ public class BottleController {
         return bottleFacade.pickRandomBottle();
     }
 
+    @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping(path = "/bottle")
     public BottleResponse createBottle(@RequestBody CreateBottleRequest request, @UserId Long userId) {
         return bottleFacade.createBottle(request.streamingMusicUrl(), userId);

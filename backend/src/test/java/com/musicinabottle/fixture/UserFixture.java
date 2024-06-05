583e4967d7cee7fd521af60e775b32730521cf95
2jun0 <soo28819@gmail.com>
2024-06-05 16:05:20 +0900

    test: 사용하지 않는 fixture 유저 이름 삭제

--- backend/src/test/java/com/musicinabottle/fixture/UserFixture.java
+++ backend/src/test/java/com/musicinabottle/fixture/UserFixture.java
@@ -1,8 +1,6 @@
 package com.musicinabottle.fixture;
 
 public final class UserFixture {
-    
+
     public static final String USERNAME_CAOCAO = "caocao";
-    public static final String USERNAME_LIUBEI = "유비";
-    public static final String USERNAME_GUANYU = "关羽";
 }

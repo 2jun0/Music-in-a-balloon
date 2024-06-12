2b3c9e77a01f7a7d00ce413a0a8950aa2321a59e
2jun0 <soo28819@gmail.com>
2024-06-12 22:06:45 +0900

    refactor: 위도 경도 픽스쳐를 수정한다

--- backend/src/test/java/com/musicinaballoon/fixture/PositionFixture.java
+++ backend/src/test/java/com/musicinaballoon/fixture/PositionFixture.java
@@ -4,6 +4,6 @@ import java.math.BigDecimal;
 
 public final class PositionFixture {
 
-    public static BigDecimal PYRAMID_OF_KHUFU_LAT = new BigDecimal("29.97945599431905");
-    public static BigDecimal PYRAMID_OF_KHUFU_LON = new BigDecimal("31.134281289091934");
+    public static BigDecimal PYRAMID_OF_KHUFU_LAT = new BigDecimal("29.9794559943191");
+    public static BigDecimal PYRAMID_OF_KHUFU_LON = new BigDecimal("31.1342812890919");
 }

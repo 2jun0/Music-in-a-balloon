0825cb2cd8ceafcae53507c4f86c6613891744b8
2jun0 <soo28819@gmail.com>
2024-06-17 17:52:48 +0900

    feat: balloon mock 데이터 수정

--- frontend/src/mock/data/balloonList.ts
+++ frontend/src/mock/data/balloonList.ts
@@ -1,22 +1,16 @@
+const balloons = [];
+for (let i = 0; i < 100; i++) {
+  balloons.push({
+    id: i,
+    title: 'Super Shy',
+    uploadedStreamingMusicType: 'YOUTUBE_MUSIC',
+    albumImageUrl: 'https://i.ytimg.com/vi/n7ePZLn9_lQ/sddefault.jpg',
+    baseLon: Math.random() * 360,
+    baseLat: Math.random() * 180 - 90,
+    createdAt: '2024-05-01T18:04:34.53997Z',
+  });
+}
+
 export const balloonList = {
-  balloons: [
-    {
-      id: 1,
-      title: 'Super Shy',
-      uploadedStreamingMusicType: 'YOUTUBE_MUSIC',
-      albumImageUrl: 'https://i.ytimg.com/vi/n7ePZLn9_lQ/sddefault.jpg',
-      baseLon: 31.1342812890919,
-      baseLat: 29.9794559943191,
-      createdAt: '2024-06-12T18:04:33.90407Z',
-    },
-    {
-      id: 2,
-      title: 'Super Shy',
-      uploadedStreamingMusicType: 'YOUTUBE_MUSIC',
-      albumImageUrl: 'https://i.ytimg.com/vi/n7ePZLn9_lQ/sddefault.jpg',
-      baseLon: 2.294694,
-      baseLat: 48.858093,
-      createdAt: '2024-06-12T18:04:34.53997Z',
-    },
-  ],
+  balloons,
 };

037f22d2522def50fdbf27021c36ca4d67840995
2jun0 <soo28819@gmail.com>
2024-06-20 22:57:46 +0900

    feat: 항상 스크롤 위로 올림

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Error from '@component/common/Error/Error';
 import ErrorBoundary from '@component/common/ErrorBoundary/ErrorBoundary';
 import Geolocation from '@component/common/Geolocation/Geolocation';
 import Register from '@component/common/Register/Register';
+import ScrollTop from '@component/common/ScrollTop/ScrollTop';
 import Footer from '@component/layout/Footer/Footer';
 import Header from '@component/layout/Header/Header';
 
@@ -14,6 +15,7 @@ const App = () => {
 
   return (
     <ErrorBoundary Fallback={Error} onReset={handleErrorReset}>
+      <ScrollTop />
       <Geolocation>
         <Register>
           <Header />

7f63ee2d40df4d17d51304274140a7ddbf20d790
2jun0 <soo28819@gmail.com>
2024-06-24 17:27:33 +0900

    refactor: balloon controller에서 status code 명시

--- backend/src/main/java/com/musicinaballoon/balloon/presentation/BalloonController.java
+++ backend/src/main/java/com/musicinaballoon/balloon/presentation/BalloonController.java
@@ -9,12 +9,12 @@ import com.musicinaballoon.balloon.application.response.BalloonResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -24,24 +24,27 @@ public class BalloonController {
     private final BalloonFacade balloonFacade;
 
     @GetMapping(path = "/balloon/{balloonId}")
-    public BalloonResponse getBalloon(@PathVariable("balloonId") Long balloonId) {
-        return balloonFacade.getBalloon(balloonId);
+    public ResponseEntity<BalloonResponse> getBalloon(@PathVariable("balloonId") Long balloonId) {
+        BalloonResponse balloon = balloonFacade.getBalloon(balloonId);
+        return ResponseEntity.ok(balloon);
     }
 
     @PostMapping(path = "/balloon/{balloonId}/pick")
-    public BalloonResponse pickBalloon(@PathVariable("balloonId") Long balloonId,
+    public ResponseEntity<BalloonResponse> pickBalloon(@PathVariable("balloonId") Long balloonId,
             @Valid @RequestBody PickBalloonRequest request, @UserId Long userId) {
-        return balloonFacade.pickBalloon(balloonId, userId, request);
+        BalloonResponse balloon = balloonFacade.pickBalloon(balloonId, userId, request);
+        return ResponseEntity.ok(balloon);
     }
 
-    @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping(path = "/balloon")
-    public BalloonResponse createBalloon(@Valid @RequestBody CreateBalloonRequest request, @UserId Long userId) {
-        return balloonFacade.createBalloon(request, userId);
+    public ResponseEntity<BalloonResponse> createBalloon(@Valid @RequestBody CreateBalloonRequest request, @UserId Long userId) {
+        BalloonResponse balloon = balloonFacade.createBalloon(request, userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(balloon);
     }
 
     @GetMapping(path = "/balloon/list")
-    public BalloonListResponse getBalloonList(@RequestParam(name = "page") Integer page) {
-        return balloonFacade.getBalloonList(page);
+    public ResponseEntity<BalloonListResponse> getBalloonList(@RequestParam(name = "page") Integer page) {
+        BalloonListResponse balloonList = balloonFacade.getBalloonList(page);
+        return ResponseEntity.ok(balloonList);
     }
 }

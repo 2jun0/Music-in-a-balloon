ce2795537265356d0a1bf5560c2b8363d9974eb5
2jun0 <soo28819@gmail.com>
2024-06-27 17:26:59 +0900

    feat: 풍선목록은 풍선의 기준위치를 응답한다

--- backend/src/main/java/com/musicinaballoon/balloon/application/response/BalloonListItemResponse.java
+++ backend/src/main/java/com/musicinaballoon/balloon/application/response/BalloonListItemResponse.java
@@ -2,6 +2,7 @@ package com.musicinaballoon.balloon.application.response;
 
 import com.musicinaballoon.balloon.domain.Balloon;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import lombok.Builder;
 
@@ -12,6 +13,10 @@ public record BalloonListItemResponse(
         @Schema(example = "Super Shy")
         String title,
 
+        @Schema(example = "29.9794559943191")
+        BigDecimal baseLon,
+        @Schema(example = "31.1342812890919")
+        BigDecimal baseLat,
         @Schema(example = "2024-06-27T07:42:02.872Z")
         ZonedDateTime basedAt,
 
@@ -25,7 +30,11 @@ public record BalloonListItemResponse(
         return BalloonListItemResponse.builder()
                 .id(balloon.getId())
                 .title(balloon.getMusicTitle())
+
+                .baseLat(balloon.getBaseLat())
+                .baseLon(balloon.getBaseLon())
                 .basedAt(balloon.getBasedAt())
+
                 .createdAt(balloon.getCreatedAt())
                 .updatedAt(balloon.getUpdatedAt())
                 .build();

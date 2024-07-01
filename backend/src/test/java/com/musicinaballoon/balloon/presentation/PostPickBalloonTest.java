be33eefbd8fe40a6b12e45648f15898a79658fc2
2jun0 <soo28819@gmail.com>
2024-07-01 18:21:06 +0900

    test: 너무 멀리있는 풍선을 주울 시 400 에러코드를 반환한다

--- backend/src/test/java/com/musicinaballoon/balloon/presentation/PostPickBalloonTest.java
+++ backend/src/test/java/com/musicinaballoon/balloon/presentation/PostPickBalloonTest.java
@@ -1,13 +1,18 @@
 package com.musicinaballoon.balloon.presentation;
 
+import static com.musicinaballoon.fixture.BalloonFixture.DEFAULT_MESSAGE;
 import static com.musicinaballoon.fixture.BalloonFixture.DEFAULT_REPLY_MESSAGE;
+import static com.musicinaballoon.fixture.PositionFixture.EIFFEL_TOWER_LAT;
+import static com.musicinaballoon.fixture.PositionFixture.EIFFEL_TOWER_LON;
 import static com.musicinaballoon.fixture.PositionFixture.PYRAMID_OF_KHUFU_LAT;
 import static com.musicinaballoon.fixture.PositionFixture.PYRAMID_OF_KHUFU_LON;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import com.musicinaballoon.balloon.application.request.PickBalloonRequest;
 import com.musicinaballoon.balloon.application.response.BalloonResponse;
 import com.musicinaballoon.balloon.domain.Balloon;
+import com.musicinaballoon.music.domain.StreamingMusicType;
 import com.musicinaballoon.music.domain.YoutubeMusic;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -73,5 +78,34 @@ public class PostPickBalloonTest extends BalloonControllerTest {
                     softly.assertThat(response2.statusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
                 }
         );
+    }
+
+    @Test
+    @DisplayName("너무 멀리 있는 풍선을 주울 시 400 에러 코드를 반환한다")
+    void pickTooFarBalloon() {
+        // given
+        Balloon balloon = createTooFarBalloon();
+        PickBalloonRequest request = new PickBalloonRequest(DEFAULT_REPLY_MESSAGE, PYRAMID_OF_KHUFU_LAT, PYRAMID_OF_KHUFU_LON);
+
+        // when
+        ExtractableResponse<Response> response = postPickBalloon(balloon.getId(), defaultUser.getId(), request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    private Balloon createTooFarBalloon() {
+        YoutubeMusic youtubeMusic = createDefaultYoutubeMusic();
+
+        Balloon balloon = Balloon.builder()
+                .uploadedStreamingMusicType(StreamingMusicType.YOUTUBE_MUSIC)
+                .youtubeMusic(youtubeMusic)
+                .creator(defaultUser)
+                .baseLat(EIFFEL_TOWER_LAT)
+                .baseLon(EIFFEL_TOWER_LON)
+                .message(DEFAULT_MESSAGE)
+                .build();
+        balloonRepository.save(balloon);
+        return balloon;
     }
 }

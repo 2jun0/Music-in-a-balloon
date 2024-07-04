c25dd5dc6574b9f272e56238107d45a91a49c573
2jun0 <soo28819@gmail.com>
2024-07-04 16:47:48 +0900

    test: 줍지않은 풍선인 경우, BadRequest 를 응답한다.

--- backend/src/test/java/com/musicinaballoon/balloon/presentation/BalloonControllerTest.java
+++ backend/src/test/java/com/musicinaballoon/balloon/presentation/BalloonControllerTest.java
@@ -1,10 +1,13 @@
 package com.musicinaballoon.balloon.presentation;
 
 import static com.musicinaballoon.fixture.BalloonFixture.youtubeMusicBalloonBuilder;
+import static com.musicinaballoon.fixture.BalloonPickedFixture.balloonPickedBuilder;
 import static com.musicinaballoon.fixture.MusicFixture.youtubeMusicBuilder;
 
 import com.musicinaballoon.IntegrationTest;
 import com.musicinaballoon.balloon.domain.Balloon;
+import com.musicinaballoon.balloon.domain.BalloonPicked;
+import com.musicinaballoon.balloon.repository.BalloonPickedRepository;
 import com.musicinaballoon.balloon.repository.BalloonRepository;
 import com.musicinaballoon.music.domain.YoutubeMusic;
 import com.musicinaballoon.music.repository.YoutubeMusicRepository;
@@ -18,11 +21,18 @@ abstract class BalloonControllerTest extends IntegrationTest {
     @Autowired
     protected BalloonRepository balloonRepository;
 
+    @Autowired
+    protected BalloonPickedRepository balloonPickedRepository;
+
     protected Balloon createDefaultBalloon(YoutubeMusic youtubeMusic) {
         return balloonRepository.save(youtubeMusicBalloonBuilder(youtubeMusic, defaultUser).build());
     }
 
     protected YoutubeMusic createDefaultYoutubeMusic() {
         return youtubeMusicRepository.save(youtubeMusicBuilder().build());
+    }
+
+    protected BalloonPicked createBalloonPicked(Balloon balloon) {
+        return balloonPickedRepository.save(balloonPickedBuilder(balloon, defaultUser).build());
     }
 }
--- backend/src/test/java/com/musicinaballoon/balloon/presentation/PostReactBalloonTest.java
+++ backend/src/test/java/com/musicinaballoon/balloon/presentation/PostReactBalloonTest.java
@@ -3,7 +3,6 @@ package com.musicinaballoon.balloon.presentation;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.musicinaballoon.balloon.application.request.ReactBalloonRequest;
-import com.musicinaballoon.balloon.application.response.BalloonResponse;
 import com.musicinaballoon.balloon.domain.Balloon;
 import com.musicinaballoon.balloon.domain.BalloonReactType;
 import com.musicinaballoon.music.domain.YoutubeMusic;
@@ -36,13 +35,28 @@ public class PostReactBalloonTest extends BalloonControllerTest {
         // given
         YoutubeMusic youtubeMusic = createDefaultYoutubeMusic();
         Balloon balloon = createDefaultBalloon(youtubeMusic);
+        createBalloonPicked(balloon);
         ReactBalloonRequest request = new ReactBalloonRequest(BalloonReactType.BALLOON);
 
         // when
         ExtractableResponse<Response> response = postReactBalloon(balloon.getId(), request);
-        BalloonResponse balloonResponse = response.as(BalloonResponse.class);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("풍선을 줍지 않았으면 BadRequest 를 응답한다.")
+    void postReactBalloon_NotPickedBalloon_ResponsesBadRequest() {
+        // given
+        YoutubeMusic youtubeMusic = createDefaultYoutubeMusic();
+        Balloon balloon = createDefaultBalloon(youtubeMusic);
+        ReactBalloonRequest request = new ReactBalloonRequest(BalloonReactType.BALLOON);
+
+        // when
+        ExtractableResponse<Response> response = postReactBalloon(balloon.getId(), request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }

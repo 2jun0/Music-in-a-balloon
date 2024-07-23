3d1b748b572d5729c59385b2569259edd65cf18a
2jun0 <soo28819@gmail.com>
2024-07-23 18:20:14 +0900

    feat: SseEmitter 를 구독하거나 알림을 보낸다.

---
+++ backend/src/main/java/com/musicinaballoon/notification/application/ReactionNotificationFacade.java
@@ -0,0 +1,77 @@
+package com.musicinaballoon.notification.application;
+
+import com.musicinaballoon.balloon.domain.BalloonReaction;
+import com.musicinaballoon.notification.application.response.ReactionNotificationResponse;
+import com.musicinaballoon.notification.domain.ReactionNotification;
+import com.musicinaballoon.user.application.UserService;
+import com.musicinaballoon.user.domain.User;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RequiredArgsConstructor
+public class ReactionNotificationFacade {
+
+    private final SseEmitterService sseEmitterService;
+    private final ReactionNotificationService reactionNotificationService;
+    private final UserService userService;
+
+    private static String makeEventId(ReactionNotification notification) {
+        return notification.getCreatedAt().toString();
+    }
+
+    private static ZonedDateTime parseLastReactionNotificationCreatedAt(String lastEventId) {
+        return ZonedDateTime.parse(lastEventId);
+    }
+
+    private static void sendDummyEvent(SseEmitter sseEmitter) {
+        try {
+            sseEmitter.send(SseEmitter.event()
+                    .id("0")
+                    .name("Dummy")
+                    .data("Dummy message"));
+        } catch (IOException e) {
+            sseEmitter.complete();
+        }
+    }
+
+    private static void sendEvent(SseEmitter sseEmitter, ReactionNotification reactionNotification) {
+        try {
+            sseEmitter.send(SseEmitter.event()
+                    .id(makeEventId(reactionNotification))
+                    .name("Reaction-Notification")
+                    .data(ReactionNotificationResponse.from(reactionNotification)));
+        } catch (IOException e) {
+            sseEmitter.complete();
+        }
+    }
+
+    public SseEmitter subscribe(Long userId, String lastEventId) {
+        userService.validateUserExisted(userId);
+        ZonedDateTime lastReactionNotificationCreatedAt = parseLastReactionNotificationCreatedAt(lastEventId);
+
+        SseEmitter sseEmitter = sseEmitterService.createSseEmitter(userId);
+        sendDummyEvent(sseEmitter);
+
+        List<ReactionNotification> lostNotifications = reactionNotificationService.getLostNotifications(userId,
+                lastReactionNotificationCreatedAt);
+
+        for (ReactionNotification lostNotification : lostNotifications) {
+            sendEvent(sseEmitter, lostNotification);
+        }
+
+        return sseEmitter;
+    }
+
+    public void sendNotification(User receiver, BalloonReaction balloonReaction) {
+        ReactionNotification reactionNotification = reactionNotificationService.createReactionNotification(receiver,
+                balloonReaction);
+
+        if (sseEmitterService.existsEmitter(receiver.getId())) {
+            SseEmitter sseEmitter = sseEmitterService.getSseEmitter(receiver.getId());
+            sendEvent(sseEmitter, reactionNotification);
+        }
+    }
+}

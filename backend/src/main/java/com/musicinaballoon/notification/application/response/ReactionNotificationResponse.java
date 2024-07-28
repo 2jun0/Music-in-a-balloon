82d70affa8e3e5ad60a387d82f6914436d9ee2c3
2jun0 <soo28819@gmail.com>
2024-07-29 01:46:45 +0900

    feat: ReactionNotificationResponse 는 유저 이름과 풍선 정보를 포함한다.

--- backend/src/main/java/com/musicinaballoon/notification/application/response/ReactionNotificationResponse.java
+++ backend/src/main/java/com/musicinaballoon/notification/application/response/ReactionNotificationResponse.java
@@ -1,8 +1,10 @@
 package com.musicinaballoon.notification.application.response;
 
+import com.musicinaballoon.balloon.domain.Balloon;
 import com.musicinaballoon.balloon.domain.BalloonReaction;
 import com.musicinaballoon.balloon.domain.BalloonReactionType;
 import com.musicinaballoon.notification.domain.ReactionNotification;
+import com.musicinaballoon.user.application.response.UserResponse;
 import java.time.ZonedDateTime;
 import lombok.Builder;
 
@@ -10,7 +12,8 @@ import lombok.Builder;
 public record ReactionNotificationResponse(
         Long id,
         Long receiverId,
-        Long balloonId,
+        BalloonResponse balloon,
+        UserResponse responder,
         BalloonReactionType reactionType,
         ZonedDateTime createdAt
 ) {
@@ -21,9 +24,26 @@ public record ReactionNotificationResponse(
         return ReactionNotificationResponse.builder()
                 .id(reactionNotification.getId())
                 .receiverId(reactionNotification.getReceiver().getId())
-                .balloonId(balloonReaction.getBalloon().getId())
+                .balloon(BalloonResponse.from(balloonReaction.getBalloon()))
                 .reactionType(balloonReaction.getType())
                 .createdAt(reactionNotification.getCreatedAt())
+                .responder(UserResponse.from(balloonReaction.getUser()))
                 .build();
+    }
+
+    @Builder
+    record BalloonResponse(
+            Long id,
+            String title,
+            String colorCode
+    ) {
+
+        static BalloonResponse from(Balloon balloon) {
+            return BalloonResponse.builder()
+                    .id(balloon.getId())
+                    .title(balloon.getMusicTitle())
+                    .colorCode(balloon.getColorCode())
+                    .build();
+        }
     }
 }

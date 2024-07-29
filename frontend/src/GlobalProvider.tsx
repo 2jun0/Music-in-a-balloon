7bd92d9105e26410a9c8d9d93d2dc43674f03cc8
2jun0 <soo28819@gmail.com>
2024-07-29 17:56:45 +0900

    refactor: 전역 스타일 제거

--- frontend/src/GlobalProvider.tsx
+++ frontend/src/GlobalProvider.tsx
@@ -1,9 +1,7 @@
-import { Global, ThemeProvider } from '@emotion/react';
+import { ThemeProvider } from '@emotion/react';
 import type { PropsWithChildren } from 'react';
 
 import ToastProvider from '@component/ToastProvider/ToastProvider';
-
-import { GlobalStyle } from '@style/GlobalStyle';
 
 import { theme } from './style/Theme';
 
@@ -11,7 +9,6 @@ type GlobalProviderProps = PropsWithChildren;
 
 const GlobalProvider = ({ children }: GlobalProviderProps) => (
   <ThemeProvider theme={theme}>
-    <Global styles={GlobalStyle} />
     {children}
     <ToastProvider />
   </ThemeProvider>
